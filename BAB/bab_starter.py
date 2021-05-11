721e705b324e3e93d63675b35fd72577be4ba183
Lilo Heinrich <lheinrich@olin.edu>
2021-05-11 00:33:10 -0400

    Finished branch & bound, passes all tests!

--- BAB/bab_starter.py
+++ BAB/bab_starter.py
@@ -15,13 +15,13 @@ class BBTreeNode():
         self.objective = objective
         self.prob = prob
 
-    def deepcopy(self, thing):
+    def deepcopy(self, memo):
         '''
         Deepcopies the picos problem
         This overrides the system's deepcopy method bc it doesn't work on classes by itself
         '''
-        newprob = pic.Problem.clone(thing.prob) #, copyOptions=True)
-        return BBTreeNode(thing.vars, newprob.constraints, thing.objective, newprob)
+        newprob = pic.Problem.clone(memo.prob) #, copyOptions=True)
+        return BBTreeNode(memo.vars, newprob.constraints, memo.objective, newprob)
 
     def buildProblem(self):
         '''
@@ -50,24 +50,20 @@ class BBTreeNode():
             return False
         return True
 
-    def branch_floor(self, var):
+    def branch_floor(self, branch_var):
         '''
         Makes a child where xi <= floor(xi)
         '''
         n1 = self.deepcopy(self)
-        branch_var = var
-        # print("floor =", math.floor(branch_var), " -- branch_var.value =", branch_var)
         n1.prob.add_constraint(branch_var <= math.floor(branch_var)) # add in the new binary constraint
         n1.constraints = list(n1.constraints.values())
         return n1
 
-    def branch_ceil(self, var):
+    def branch_ceil(self, branch_var):
         '''
         Makes a child where xi >= ceiling(xi)
         '''
         n2 = self.deepcopy(self)
-        branch_var = var
-        # print("ceiling =", math.ceil(branch_var), " -- branch_var.value =", branch_var)
         n2.prob.add_constraint(branch_var >= math.ceil(branch_var)) # add in the new binary constraint
         n2.constraints = list(n2.constraints.values())
         return n2
@@ -82,43 +78,29 @@ class BBTreeNode():
               bestnode_vars = the list of variables that create bestres
         '''
 
-        # build up the initial problem and adds it to a heap
+        # build up initial problem and add it to heap
         root = self
         heap = [(next(counter), root)]
         bestres = -1e20 # a small arbitrary initial best objective value
         bestnode_vars = root.vars # initialize bestnode_vars to the root vars
         bestres_final = bestres
         while len(heap) > 0:
-            # for h in heap:
-            #     print("on heap:", h)
-            #     for v in h[1].vars:
-            #         print("\t", v.name, "=", (v.value,4))
-
             count, root = heap.pop()
             try:
                 res = root.buildProblem().solve(solver='cvxopt')
             except:
                 continue
 
-            # print("root.vars =", [i.value for i in root.vars])
-            # print("res.value =", res.value, "@ vars =",[res.problem.variables[i].value for i in res.problem.variables])
-
-            # cut this branch
-            if res.value <= bestres_final: continue
-
-            # this is a leaf node, found an integral solution!
-            if root.is_integral():
+            if res.value <= bestres_final: continue # less than optimal, cut branch
+            if root.is_integral(): # leaf node, found an integral solution!
                 bestnode_vars = [i.value for i in root.vars]
                 bestres_final = res.value
-                # print("bestres_final =", bestres_final, "@ vars =",bestnode_vars)
                 continue
 
-            # print("push more branches")
             i = 0
             while root.is_var_integral(root.vars[i]):
                 i += 1
 
-            # print("VAR NAME =" ,root.vars[i].name)
             root_floor = root.branch_floor(root.vars[i])
             floor_prob = root_floor.buildProblem()
             root_ceil = root.branch_ceil(root.vars[i])
